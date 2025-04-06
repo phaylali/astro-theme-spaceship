521534659a970b4dcccbfab814e72acf2b267b09
aitorllamas <aitorllj93@gmail.com>
2025-04-06 17:31:52 +0200

    deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://aitorllj93.github.io',
+  base: 'astro-theme-spaceship',
   integrations: [],
   markdown: {
     shikiConfig: {
