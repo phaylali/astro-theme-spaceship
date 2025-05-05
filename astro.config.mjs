2685465aac530010b415c1f1c8eae3a118b8b81e
aitorllamas <aitorllj93@gmail.com>
2025-05-05 10:59:23 +0200

    fix: Request URLs for public/ assets must also include your base

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import websiteConfig from './website.config.mjs';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aitorllj93.github.io',
-  base: 'astro-theme-spaceship',
+  base: websiteConfig.base,
   i18n: {
     defaultLocale: websiteConfig.defaultLanguage,
     locales: [websiteConfig.defaultLanguage],
