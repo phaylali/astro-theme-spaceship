47da879cae42f96a1d69dc2f0f4900009817fbf9
aitorllamas <aitorllj93@gmail.com>
2025-07-12 14:44:35 +0200

    minor ui changes

--- website.config.mjs
+++ website.config.mjs
@@ -3,6 +3,6 @@ export default {
   author: 'Spaceship CO',
   base: 'astro-theme-spaceship',
   title: 'Spaceship',
-  description: 'Astro Template for publishing your Obsidian Vault',
+  description: 'Ship your Second Brain into the Cyber-Space',
   defaultLanguage: 'en',
 }
