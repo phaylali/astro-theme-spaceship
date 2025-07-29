107efdf0bbff019f501f3eb0107618f871defeef
aitorllamas <aitorllj93@gmail.com>
2025-07-29 20:29:50 +0200

    move shell and theme to spaceship and monolith packages

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,7 @@
 
 import { defineConfig } from 'astro/config';
-
-import { create } from './spaceship/shell';
+import { create } from 'astro-spaceship';
 
 import websiteConfig from './website.config.json';
 
-export default defineConfig({
-  ...create(websiteConfig),
-});
+export default defineConfig(create(websiteConfig));
